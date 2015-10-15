e534ed1a83e45843e3c002de0fd0ccd8cf5fd21f
Oddvar Lovaas <oddvar@matrix.org>
2015-10-15 11:49:03 +0100

    offset internal links in the spec correctly

--- jekyll/add_anchors.py
+++ jekyll/add_anchors.py
@@ -5,7 +5,13 @@ script, filename = argv
 
 textfile = open(filename, "r")
 regex = r'(<h\d id="(.*?)">)'
+regex2 = r'(<div class="section" id="(.*?)">)'
+
 replacement = r'<p><a class="anchor" id="\2"></a></p>\n\1'
 
+# check for <hX id="..." (used in the FAQ) and add anchors - and then check 
+# for <div class="section" id="..." and add anchors (used in the spec)
 for line in textfile:
-  print(re.sub(regex, replacement, line.rstrip()))
+  line = re.sub(regex, replacement, line.rstrip())
+  print(re.sub(regex2, replacement, line.rstrip()))
+
