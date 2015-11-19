ebf0fb292e57f03267cb4c0234ef9b3448352c57
Daniel Wagner-Hall <dawagner@gmail.com>
2015-11-19 17:46:55 -0500

    Add script for generating documentation
    The new instructions for generating the docs will simply read:
    
     * Go to checkout of website on serving machine
     * Run git submodule update --remote
     * Run ./scripts/update_docs.sh
    
    Things I don't like about this change as-is:
     * The includes/from_jekyll directory - presumably these files should
       actually live somewhere useful?

---
+++ scripts/update_docs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+# Note that this file is world-readable unless someone plays some .htaccess hijinks
+
+echo >&2 "Make sure you have run \`git submodule update --remote\` to pull in the latest changes."
+
+SELF="${BASH_SOURCE[0]}"
+if [[ "${SELF}" != /* ]]; then
+  SELF="$(pwd)/${SELF}"
+fi
+SELF="${SELF/\/.\///}"
+cd "$(dirname "$(dirname "${SELF}")")"
+
+SITE_BASE="$(pwd)"
+
+cd matrix-doc/scripts
+INCLUDES="${SITE_BASE}/includes/from_jekyll"
+python gendoc.py
+./add-matrix-org-stylings.sh "${INCLUDES}/head.html" "${INCLUDES}/nav.html" "${INCLUDES}/footer.html"
+
+mkdir -p "${SITE_BASE}/docs/"{api/client-server/json,howtos,spec}
+cp gen/specification.html  "${SITE_BASE}/docs/spec/index.html"
+cp gen/howtos.html "${SITE_BASE}/docs/howtos/client-server.html"
+for f in "${SITE_BASE}"/matrix-doc/api/client-server/*; do
+  if [[ -f "${f}" ]]; then
+    cp "${f}" "${SITE_BASE}/docs/api/client-server/json/"
+  fi
+done
