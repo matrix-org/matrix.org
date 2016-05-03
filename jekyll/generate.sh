10171f30fbe73f2957a2c54aab66091ed785ab63
Oddvar Lovaas <oddvar@matrix.org>
2016-05-03 12:47:07 +0100

    local uncommitted changes

--- jekyll/generate.sh
+++ jekyll/generate.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 jekyll build
-python add_anchors.py _site/faq.html >tmp && mv tmp _site/faq.html
+python add_anchors.py _site/guides/faq.html >tmp && mv tmp _site/guides/faq.html
+python add_anchors.py _site/projects/try-matrix-now.html >tmp && mv tmp _site/projects/try-matrix-now.html
+
--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -32,9 +32,15 @@ python gendoc.py -c "${client_release_label}"
 ./add-matrix-org-stylings.sh "${INCLUDES}" gen/*.html
 )
 
+cd /home/matrixsite/matrix.org/jekyll
+./generate.sh
+cd _site/guides
+ln -s ../css
+cd ../../..
+
 cp matrix-doc/scripts/gen/* "docs/spec/${client_release_label}"
 cp matrix-doc/scripts/gen/howtos.html docs/howtos/client-server.html
-cp jekyll/css/docs_overrides.css docs/guides/css/docs_overrides.css
+cp jekyll/css/docs_overrides.css docs/css/docs_overrides.css
 
 cp -r swagger-ui/dist/* docs/api/client-server/
 matrix-doc/scripts/dump-swagger.py "${SITE_BASE}/docs/api/client-server/json" "${client_release_label}"
@@ -46,3 +52,4 @@ patch -p0 <scripts/swagger-js-654.patch
 
 echo "generating docs/spec/olm.html"
 rst2html.py olm/docs/olm.rst > docs/spec/olm.html
+
