matrix-org/matrix.org

commit 90ef063746f10f690dee0e326435f6fc59a5c8d4

uncommitted changes

scripts/update_docs.sh
@@ -43,9 +43,9 @@ find "$SCRIPTS_DIR/gen" -name '*.html' |
     xargs "$SCRIPTS_DIR/add-matrix-org-stylings.pl" "${INCLUDES}"
 
 # move the generated docs into docs/
-mkdir -p docs/howtos
-mv "$SCRIPTS_DIR/gen/howtos.html" docs/howtos/client-server.html
-cp -r "$SCRIPTS_DIR"/gen/* docs/spec
+#mkdir -p docs/howtos
+#mv "$SCRIPTS_DIR/gen/howtos.html" docs/howtos/client-server.html
+#cp -r "$SCRIPTS_DIR"/gen/* docs/spec
 
 
 # now update other bits of the site

packages/debian/conf/distributions
@@ -51,3 +51,16 @@ Components: main
 SignWith: packages@matrix.org
 Tracking: minimal
 
+Origin: matrix.org
+Codename: xenial
+Architectures: amd64 i386 source
+Components: main
+SignWith: packages@matrix.org
+Tracking: minimal
+
+Origin: matrix.org
+Codename: yakkety
+Architectures: amd64 i386 source
+Components: main
+SignWith: packages@matrix.org
+Tracking: minimal

commit 9fb19f7341c398ab58598d8de34d1d9a4657eac1

Actually update the spec docs when updating the site
Undo the breakage caused by 90ef063, so that the spec docs are in fact updated.

scripts/update_docs.sh
@@ -44,12 +44,10 @@ find "$SCRIPTS_DIR/gen" -name '*.html' |
     xargs "$SCRIPTS_DIR/add-matrix-org-stylings.pl" "${INCLUDES}"
 
 # move the generated docs into docs/
-#mkdir -p docs/howtos
-#mv "$SCRIPTS_DIR/gen/howtos.html" docs/howtos/client-server.html
-#cp -r "$SCRIPTS_DIR"/gen/* docs/spec
+cp -r "$SCRIPTS_DIR"/gen/* docs/spec
 
-
-# now update other bits of the site
+# now update other bits of the site (guides, projects, etc).
+# This will generte stuff under ./jekyll/_site, which is symlinked into ./docs/.
 ./jekyll/generate.sh
 
 echo "generating olm specs"