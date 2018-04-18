0ebfe820afa177127981a26ac5e5ceac903a539a
Ben Parsons <ben@bpulse.co.uk>
2018-04-18 10:28:28 +0100

    maintenance script for old md files

---
+++ scripts/read-existing-md.js
@@ -0,0 +1,35 @@
+var fs = require('fs'),
+readline = require('readline');
+
+var files = fs.readdirSync('../jekyll/_posts/projects');
+console.log("<table>");
+
+files.forEach(file => {
+    if (file === "images") return;
+    console.log("<tr>");
+
+    console.log("<td>");
+    console.log(file);
+
+    console.log("</td>");
+
+    console.log("<td>");
+    var titleFound = false;
+    var lines = fs.readFileSync('../jekyll/_posts/projects/' + file, 'utf-8').split('\n');
+    var content = "";
+    lines.forEach(line => {
+      if (titleFound) {
+        if (line.length === 0) {
+          content += "<br />";
+        } else {
+          content += line + "<br />";
+        }
+        
+      }
+      if (line === "# {{ page.title }}") titleFound = true;
+    });
+    console.log(content);
+    
+    console.log("</td>");
+    console.log("</tr>");
+  });
