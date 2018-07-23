9985eb81bb514a531be996e4d254a60939042af6
Ben Parsons <ben@bpulse.co.uk>
2018-07-23 15:02:50 +0100

    update CSS requirements and latest set of questions

--- jekyll/generate.sh
+++ jekyll/generate.sh
@@ -7,8 +7,6 @@ export LANG="en_GB.UTF-8"
 
 cd `dirname $0`
 
-node ../scripts/generate-faq.js > _posts/guides/2015-08-19-faq.md
-
 jekyll build
 
 ./add_anchors.py _site/projects/try-matrix-now.html
