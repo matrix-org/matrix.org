4ccda621f62047f459ac314b9d4bb2a02ca24e7b
Ben Parsons <ben@bpulse.co.uk>
2019-02-06 17:23:48 +0000

    pretty-faq

---
+++ scripts/pretty-faq.js
@@ -0,0 +1,110 @@
+const fs = require('fs');
+var MarkdownIt = require('markdown-it');
+var md = new MarkdownIt({html: true});
+var markdownItAttrs = require('markdown-it-attrs'); 
+md.use(markdownItAttrs);
+var markdownItAnchor = require("markdown-it-anchor");
+const mapping = { h2: 'section-title', h3: 'question', p: 'answer', ul: 'answer' };
+md.use(markdownItAnchor, {
+    permalink: true,
+    permalinkSymbol: "&#128279;",
+    permalinkClass: "permalink",
+    permalinkBefore: true
+}).use(require('@toycode/markdown-it-class'), mapping);
+
+process.chdir(__dirname);
+
+var htmlOutput = html();
+console.log(htmlOutput);
+fs.writeFileSync(`/Users/benp/projects/PrettyDocs-Theme/faq-out.html`, htmlOutput);
+function html() {
+    var root = "../content/faq/";
+
+    var outline = fs.readFileSync("/Users/benp/projects/PrettyDocs-Theme/template-faq.html", 'utf-8');
+
+    var markdownSource = "";
+    var topMenuHtmlSource = "";
+
+    var config = JSON.parse(fs.readFileSync(root + "questions-config.json", 'utf8'));
+
+    var definitionLinks = "";
+    config.definitions.forEach((definition) => {
+        definitionLinks += "[" + definition.name.toLowerCase() + "]: ";
+        definitionLinks += "#definition-" + definition.name.toLowerCase().replace(' ', '');
+        definitionLinks += "\n";
+    });
+
+    config.sections.forEach((section) => {
+        //markdownSource += "\n# " + section.name + "\n";
+        //topMenuHtmlSource += '<div class="top-menu-item"><span class="title"><a href="#' + markdownItAnchor.defaults.slugify(section.name) + '">' + section.name + '</a></span><br />';
+        section.parts.forEach((part) => {
+            markdownSource += "\n## " + part.name + "\n\n";
+            topMenuHtmlSource += '<a class="nav-link scrollto" href="#' + markdownItAnchor.defaults.slugify(part.name) + '">' + part.name + '</a>';
+            part.questions.forEach((question) => {
+                markdownSource += '<div class="section-block">\n\n';
+                if (typeof(question) == 'number') { question = "q" + question; } 
+                var questionMarkdown = fs.readFileSync(root + "questions/" + question + ".md", 'utf-8');
+
+                var questionParsed = md.parse(questionMarkdown + "\n\n" + definitionLinks, {});
+                var questionDefinitions = [];
+                questionParsed.forEach(token => {
+                    if (! token.children) {
+                        return;
+                    }
+                    token.children.forEach(childToken => {
+                        if (childToken.type === "link_open") {
+                            childToken.attrs.forEach(attr => {
+                                if (attr[1].indexOf('#definition-') !== -1)
+                                    questionDefinitions.push(attr[1].replace('#definition-', ''));
+                            });
+                        }
+                    });
+                });
+
+                markdownSource += "\n<div class='question'>\n\n";
+
+                questionMarkdown += "\n\n<div class='definition-list'>\n\n";
+                
+                questionDefinitions.forEach(term => {
+                    var definition = config.definitions.find(d => {return d.name.toLowerCase().replace(' ', '') == term.toLowerCase()})
+                    questionMarkdown += "\n\n<div class='definition-item definition-" + term + "'>\n\n";
+                    questionMarkdown += "**" + definition.name + "**\n\n";
+                    questionMarkdown += definition.definition;
+                    questionMarkdown += "\n\n</div>\n\n";
+                });
+
+                questionMarkdown += "<div class='definition-close'>close</div>";
+
+                questionMarkdown += "\n\n</div><!--.definition-list-->\n\n";
+
+                markdownSource += questionMarkdown;
+                markdownSource += "\n</div>\n";
+                markdownSource += "\n</div>\n";
+            });
+        });
+        //topMenuHtmlSource += '</div>';
+    });
+
+    var definitionsTable = '\n\n\# Definitions\n\nTerm | Definition\n--- | ---\n';
+    config.definitions.forEach((definition) => {
+        definitionsTable += definition.name;
+        definitionsTable += '|' + definition.definition + "|\n";
+    });
+    markdownSource += "\n\n" + definitionsTable + "\n\n";
+
+    markdownSource += "\n\n";
+    markdownSource += definitionLinks;
+
+    var twimUrls = fs.readFileSync(root + "_url-directory.md", 'utf8');
+
+    markdownSource += "\n\n" + twimUrls;
+
+    outline = outline.replace("{{QUESTIONS}}", md.render(markdownSource));
+    outline = outline.replace("{{TOP-MENU}}", topMenuHtmlSource);
+
+    return outline;
+}
+
+module.exports = {
+    html: html
+}
