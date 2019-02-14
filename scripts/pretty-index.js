8a517ef0d0916a77c35ff3127ed54e6f35ec63b1
Ben Parsons <ben@bpulse.co.uk>
2019-02-14 16:18:21 +0000

    index generation script

---
+++ scripts/pretty-index.js
@@ -0,0 +1,53 @@
+#! /usr/local/bin/node
+
+const fs = require('fs');
+var newDocsPath = "/Users/benp/projects/matrix.org-docs-2019/";
+const MarkdownIt = require('markdown-it');
+const md = MarkdownIt().use(require('markdown-it-front-matter'), () => {});
+process.chdir(__dirname);
+
+
+var indexTemplate = fs.readFileSync(`${newDocsPath}/template-index.html`, 'utf-8');
+
+var cards = [];
+cards.push(new Card("how-can-i-get-involved.html", "", "Get Involved", ""));
+cards.push(new Card("usage-of-the-js-sdk.html", "", "Start Making", ""));
+cards.push(new Card("installing-synapse.html", "", "Install Synapse", ""));
+cards.push(new Card("faq-out.html", "", "FAQs", ""));
+cards.push(new Card("try-matrix-now.html", "", "Try Matrix Now", ""));
+cards.push(new Card("legal.html", "", "Legal", ""));
+
+var indexCardsHtml = "";
+cards.forEach(card => {
+    indexCardsHtml += getHomepageCard(card);
+})
+
+indexTemplate = indexTemplate.replace("<!--INDEXCARDS-->", indexCardsHtml);
+
+var guidesIndexMd = fs.readFileSync("../jekyll/_posts/guides/2016-01-01-index.md", 'utf-8');
+var guidesIndexHtml = md.render(guidesIndexMd);
+
+indexTemplate = indexTemplate.replace("<!--GUIDESLIST-->", guidesIndexHtml);
+
+fs.writeFileSync(`${newDocsPath}index.html`, indexTemplate);
+
+function getHomepageCard(card) {
+    return `
+<div class="col-md-4 col-12 mb-3">
+    <div class="theme-card">
+        <a href="${card.url}" target="_blank"><img class="img-fluid" src="${card.img}" alt="screenshot" /></a>
+        <div class="card-block">
+            <h4 class="card-title">${card.title}</h4>
+            <p class="card-text">${card.description}</p>
+        </div><!--//card-block-->
+        <a class="mask" href="${card.url}" target="_blank"><i class="icon fa fa-search-plus"></i></a>
+    </div><!--//theme-card-->
+</div>`;
+}
+
+function Card(url, img, title, description) {
+    this.url = url;
+    this.img = img;
+    this.title = title;
+    this.description = description;
+}
