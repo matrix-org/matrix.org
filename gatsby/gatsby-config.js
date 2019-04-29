e8dff79352e0b948331632bcf55a63dd9569a153
Ben Parsons <ben@bpulse.co.uk>
2019-04-29 18:01:32 +0100

    working RSS

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,6 @@
 const config = require('./config')
 
 const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
-const mdxFeed = require("gatsby-mdx/feed")
 
 
 module.exports = {
@@ -14,7 +13,53 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-plugin-feed`,
-      options: mdxFeed
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {              return allMdx.edges.map(edge => {                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(   limit: 10,               sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      },
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
