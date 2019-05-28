8d5dc255dbcbdb6c26e6b884b5c48a5907149028
Ben Parsons <ben@bpulse.co.uk>
2019-05-28 17:38:52 +0100

    init new styles dump

---
+++ gatsby/src/components/Navigation.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { darken, lighten } from 'polished'
+
+
+const Content = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+
+  a {
+    color: ${props => props.theme.colors.primary};
+    font-size: 3.2rem;
+    &:hover {
+      opacity: 0.85;
+      color: ${props => props.theme.colors.primaryLight};
+    }
+  }
+`
+
+const Navigation = () => (
+  <nav>
+<div data-collapse="medium" data-animation="default" data-duration="400" class="mxnavbar w-nav">
+    <div class="mxnavbar__container w-container"><a href="index.html" class="mxnavbar__brand w-nav-brand"><img src="/images/matrix-logo.svg" alt="" class="mxnavbar__logo" /></a>
+      <nav role="navigation" class="mxnavbar__navmenu w-nav-menu"><a href="discover.html" class="mxnavbar__navlink w-nav-link">Discover</a><a href="guides.html" class="mxnavbar__navlink w-nav-link">Guides</a><a href="develop.html" class="mxnavbar__navlink w-nav-link">Develop</a><a href="blog.html" class="mxnavbar__navlink w-nav-link w--current">Blog</a><a href="#" class="mxnavbar__navlink w-nav-link">FAQ&#x27;s</a><a href="try-now.html" class="mxnavbar__navlink mxnavbar__navlink--try w-nav-link">Try Now</a><a href="try-now.html" class="mxnavbar__navlink mxnavbar__navlink--primary w-nav-link">Try Now</a></nav>
+      <div class="mxnavbar__menubutton w-nav-button">
+        <div class="mxnavbar__icon w-icon-nav-menu"></div>
+      </div>
+    </div>
+  </div>
+  <div class="mxnavsection"><a href="http://www.google.com" class="mxnavsection__item w-inline-block"><img src="/images/basic_world.svg" alt="" class="mxnavsection__icon" /><div class="mxnavsection__text">All posts</div></a><a href="http://www.google.com" class="mxnavsection__item w-inline-block"><img src="/images/basic_elaboration_calendar_star.svg" alt="" class="mxnavsection__icon" /><div class="mxnavsection__text">This week in Matrix</div></a><a href="http://www.google.com" class="mxnavsection__item w-inline-block"><img src="/images/basic_lock.svg" alt="" class="mxnavsection__icon" /><div class="mxnavsection__text">Security</div></a><a href="http://www.google.com" class="mxnavsection__item w-inline-block"><img src="/images/basic_rss.svg" alt="" class="mxnavsection__icon" /><div class="mxnavsection__text">RSS Feed</div></a></div>
+  </nav>
+
+)
+
+export default Navigation
