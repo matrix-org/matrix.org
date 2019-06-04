a935510c7380a67d9a4aa738dca4be265e0d9578
Ben Parsons <ben@bpulse.co.uk>
2019-06-04 19:18:30 +0100

    hosting and blog archive page

---
+++ gatsby/src/pages/blog/archive.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { Link, graphql } from 'gatsby'
+import styled from 'styled-components'
+import kebabCase from 'lodash/kebabCase'
+
+import { Layout, Wrapper, Header, SectionTitle, MXContentMain } from '../../components'
+import config from '../../../config'
+
+const Content = styled.div`
+  grid-column: 2;
+  box-shadow: 0 4px 120px rgba(0, 0, 0, 0.1);
+  border-radius: 1rem;
+  padding: 2rem 4rem;
+  background-color: ${props => props.theme.colors.bg};
+  z-index: 9000;
+  @media (max-width: ${props => props.theme.breakpoints.tablet}) {
+    padding: 3rem 3rem;
+  }
+  @media (max-width: ${props => props.theme.breakpoints.phone}) {
+    padding: 2rem 1.5rem;
+  }
+`
+
+const Title = styled.h3`
+  position: relative;
+  text-shadow: 0 12px 30px rgba(0, 0, 0, 0.15);
+  margin-bottom: 0.75rem;
+`
+
+const Category = ({
+  data: {
+    allMdx: { edges },
+  },
+}) => (
+  <Layout>
+      <Helmet title={`Blog Archive ${config.siteTitle}`} />
+      <MXContentMain>
+        <h1>Blog Archive</h1>
+        {edges.map(edge => (
+          <p><a href={edge.node.fields.slug}>{edge.node.frontmatter.title}</a></p>
+          
+        ))}
+      </MXContentMain>
+  </Layout>
+)
+
+export default Category
+
+Category.propTypes = {
+  data: PropTypes.shape({
+    allMdx: PropTypes.shape({
+      group: PropTypes.array.isRequired,
+    }),
+  }).isRequired,
+}
+
+export const postQuery = graphql`
+  query ArchivePage {
+    allMdx(sort: { fields: [frontmatter___date, fileAbsolutePath], order: DESC },
+      filter: {frontmatter: {date: {ne: null}}}) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
---
+++ gatsby/src/pages/docs/projects/hosting.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Helmet from 'react-helmet'
+import styled from 'styled-components'
+import { Layout, MXContentMain, Header } from '../../../components'
+import config from '../../../../config'
+
+
+const Hosting = ({data}) => {
+ 
+    return (<Layout>
+        <MXContentMain>
+          <Helmet title={`SDKs | ${config.siteTitle}`} />
+          <h1 id="matrix-hosting">Matrix Hosting</h1>
+
+            <div class="hosting-provider">
+
+            <h2 id="modularim">Modular.im</h2>
+
+            <p><img src="/docs/projects/images/modularlogo.png" alt="" /></p>
+            <p><a href="https://www.modular.im/">www.modular.im</a></p>
+
+            <p>
+            <span class="hhs-green">✓ Hosted Homeservers</span><br />
+            <span><em>&nbsp;&nbsp;&nbsp;&nbsp;Hosted Integrations coming soon</em></span>
+            </p>
+
+            <p>Hosted Homeservers for those that don’t want to manage their own.</p>
+
+            <p>Brought to you by the creators of Matrix, who have been running the biggest homeserver in the network since 2014. Every homeserver comes with a custom instance of Riot.</p>
+          </div>
+        </MXContentMain>
+    </Layout>)
+}
+
+
+export const query = graphql`
+{
+    allFile(filter: { sourceInstanceName: { eq: "projects" } }) {
+        
+        edges {
+            node {
+                childMdx {
+                    frontmatter {
+                        title
+                        maturity
+                        description
+                        thumbnail
+                        featured
+                        categories
+                        language
+                        author
+                        repo
+                        room
+                        e2e
+                    }
+                    code {
+                      body
+                    }
+                }
+                absolutePath
+            }
+        }
+    }
+}
+`
+export default Hosting
