8ab4b32c01cd5767aadbebf12995a2607ca82196
Ben Parsons <ben@bpulse.co.uk>
2019-06-06 12:33:55 +0100

    move legal content

---
+++ gatsby/src/pages/legal.js
@@ -0,0 +1,40 @@
+/* eslint jsx-a11y/label-has-for:0 */
+
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Layout, Wrapper, Header, MXContentMain} from '../components'
+
+import config from '../../config'
+
+
+
+const Legal = () => {
+    return (<Layout navmode="develop">
+            <Helmet title={`Legal | ${config.siteTitle}`} />
+            <MXContentMain>
+          <div class="mxcontent__main__doc">
+                <h1>Legal</h1>
+                Links to our legal documentation:
+                <ul>
+                    <li>
+                        <a href="https://matrix.org/docs/guides/privacy_notice.html">Privacy Notice</a>
+                    </li>
+                    <li>
+                        <a href="https://matrix.org/docs/guides/terms_and_conditions.html">Terms and Conditions</a>
+                    </li>
+                    <li>
+                        <a href="https://matrix.org/docs/guides/code_of_conduct">Code of Conduct</a>
+                    </li>
+                    <li>
+                        <a href="https://matrix.org/docs/guides/copyright_notice">Copyright Notice</a>
+                    </li>
+                    <li>
+                        <a href="https://matrix.org/docs/guides/exceptional_erasure_policy">Exceptional Erasure Policy</a>
+                    </li>
+                </ul>
+                </div>
+            </MXContentMain>
+    </Layout>)
+}
+
+export default Legal
