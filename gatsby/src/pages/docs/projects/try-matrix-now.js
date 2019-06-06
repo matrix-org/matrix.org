3b4187a848b10071a6a8a32fe9961761ac92d6e0
Ben Parsons <ben@bpulse.co.uk>
2019-06-06 11:41:31 +0100

    tmn display filter

--- gatsby/src/pages/docs/projects/try-matrix-now.js
+++ gatsby/src/pages/docs/projects/try-matrix-now.js
@@ -22,12 +22,17 @@ const TryMatrixNow = ({data}) => {
     const others = projects.filter(project => project.frontmatter.categories[0] === "other")
 
     const languages = Array.from(new Set( projects.map(project => project.frontmatter.language) ))
+        .sort()
+        .filter(l => l !== null)
+
+    const licenses = Array.from(new Set( projects.map(project => project.frontmatter.license) ))
+        .sort()
+        .filter(l => l !== null)
     
     return (<Layout navmode="discover">
         <MXContentMain>
             <Helmet title={`Try Matrix Now | ${config.siteTitle}`} />
             <h1>Try Matrix Now</h1>
-            {JSON.stringify(languages)}
             <h2>Let's go deeper</h2>
             <p>Matrix is a whole ecosystem of matrix-enabled clients, servers, gateways, application services, bots, etc. If you’d like to learn more, this page aims to collect all known Matrix projects. To add a new one (or update an existing one), you can submit a PR to the <a href="https://github.com/matrix-org/matrix.org">matrix.org</a> project on github - the existing projects can be found <a href="https://github.com/matrix-org/matrix.org/tree/master/jekyll/_posts/projects">here</a> - or just let us know in the #matrix:matrix.org room.<br /></p>
             <div class="mxblock">
@@ -82,25 +87,35 @@ const TryMatrixNow = ({data}) => {
                     </div>
                 </div>
                 <div class="mxgrid__item40">
-                <div class="mxgrid__item__bg mxgrid__item__bg--clear mxgrid__item__bg--left_align">
-                <h4 class="mxgrid__item__bg__hx">Language</h4>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
-                </div>
+                    <div class="mxgrid__item__bg mxgrid__item__bg--clear mxgrid__item__bg--left_align">
+                        <h4 class="mxgrid__item__bg__hx">Language</h4>
+                        <div class="tmn-two-column">
+                        {
+                            languages.map(function(maturity) {
+                                const id = "chk-language-" + maturity.replace(/ /g, '')
+                                return (
+                                    <p class="mxgrid__item__bg__p">
+                                        <input type="checkbox" id={id} name={id} checked="checked" />
+                                        <label for={id}> {maturity}</label>
+                                    </p>)
+                            })
+                        }
+                        </div>
+                    </div>
                 </div>
                 <div class="mxgrid__item20">
                 <div class="mxgrid__item__bg mxgrid__item__bg--clear mxgrid__item__bg--left_align">
-                <h4 class="mxgrid__item__bg__hx">License</h4>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
-                <p class="mxgrid__item__bg__p">Item<br />
-                </p>
+                    <h4 class="mxgrid__item__bg__hx">License</h4>
+                    {
+                        licenses.map(function(maturity) {
+                            const id = "chk-license-" + maturity.replace(/ /g, '')
+                            return (
+                                <p class="mxgrid__item__bg__p">
+                                    <input type="checkbox" id={id} name={id} checked="checked" />
+                                    <label for={id}> {maturity}</label>
+                                </p>)
+                        })
+                    }
                 </div>
                 </div>
             </div>
