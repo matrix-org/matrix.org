d70a1c4e93c4c563e0596125f9c560ffef6833a8
Karol Kosek <krkkx@protonmail.com>
2019-06-11 20:39:56 +0200

    fix image in /try-now/

--- gatsby/src/pages/try-now.js
+++ gatsby/src/pages/try-now.js
@@ -16,7 +16,7 @@ const TryNow = () => {
                 <h1 class="mxblock--try__hx">Try Now</h1>
                 <p class="mxblock--try__p">The easiest way to try Matrix is to use the <a href="https://about.riot.im/" rel="noopener noreferrer" target="_blank">Riot Web</a> client in your browser, which is configured by default to connect to the matrix.org homeserver.</p>
                 
-                <img src="images/riot-ensemble.png" width="1120" srcset="images/riot-ensemble-p-500.png 500w, images/riot-ensemble-p-800.png 800w, images/riot-ensemble-p-1080.png 1080w, images/riot-ensemble.png 1803w" sizes="(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 95vw, 800px" alt="" class="mxblock--try__img" />
+                <img src="/images/riot-ensemble.png" width="1120" srcset="/images/riot-ensemble-p-500.png 500w, /images/riot-ensemble-p-800.png 800w, /images/riot-ensemble-p-1080.png 1080w, /images/riot-ensemble.png 1803w" sizes="(max-width: 479px) 96vw, (max-width: 767px) 97vw, (max-width: 991px) 95vw, 800px" alt="" class="mxblock--try__img" />
                 
                 <a href="https://about.riot.im/" rel="noopener noreferrer" target="_blank" class="mxblock__btn mxblock--hero__btn mxblock--hero_btn--try w-button">Go to Riot.im</a>
 
