94ffff0679eea03c208e384ec00b7339ef005988
Ben Parsons <ben@bpulse.co.uk>
2019-06-12 17:54:25 +0100

    remove unneeded code from Redirect pages

--- gatsby/src/pages/docs/guides/privacy_notice.js
+++ gatsby/src/pages/docs/guides/privacy_notice.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Helmet from 'react-helmet'
-import { Layout, MXContentMain } from '../../../components'
-import config from '../../../../config'
+import { Layout } from '../../../components'
 
 
-const Redirect = ({data}) => {
+const Redirect = () => {
 
     return (<Layout navmode="develop">
         <Helmet>
@@ -15,35 +13,4 @@ const Redirect = ({data}) => {
     </Layout>)
 }
 
-
-export const query = graphql`
-{
-    allFile(filter: { sourceInstanceName: { eq: "projects" } }) {
-
-        edges {
-            node {
-                childMdx {
-                    frontmatter {
-                        title
-                        maturity
-                        description
-                        thumbnail
-                        featured
-                        categories
-                        language
-                        author
-                        repo
-                        room
-                        e2e
-                    }
-                    code {
-                      body
-                    }
-                }
-                absolutePath
-            }
-        }
-    }
-}
-`
 export default Redirect
