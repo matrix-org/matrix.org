61f30f8e42b6af6da5142750b9af8b30b3d89a93
Ben Parsons <ben@bpulse.co.uk>
2019-07-04 11:41:12 +0100

    override external links with window.location

--- gatsby/src/components/Navigation.js
+++ gatsby/src/components/Navigation.js
@@ -27,15 +27,15 @@ navTypes["develop"] = (
     <img src="/images/basic_spread_text.svg" alt="" className="mxnavsection__icon" />
     <div className="mxnavsection__text">Docs</div>
   </a>
-  <a href="https://matrix.org/docs/spec" className="mxnavsection__item w-inline-block">
+  <a href="https://matrix.org/docs/spec" className="mxnavsection__item w-inline-block" onClick="window.location='https://matrix.org/docs/spec';">
     <img src="/images/basic_pencil_ruler.svg" alt="" className="mxnavsection__icon" />
     <div className="mxnavsection__text">Spec</div>
   </a>
-  <a href="https://matrix.org/docs/api/client-server/" className="mxnavsection__item w-inline-block">
+  <a href="https://matrix.org/docs/api/client-server/" className="mxnavsection__item w-inline-block" onClick="window.location='https://matrix.org/docs/api/client-server/';">
     <img src="/images/basic_cards_hearts.svg" alt="" className="mxnavsection__icon" />
     <div className="mxnavsection__text">API Playground</div>
   </a>
-  <a href="https://matrix.org/code" className="mxnavsection__item w-inline-block">
+  <a href="https://matrix.org/code" className="mxnavsection__item w-inline-block" onClick="window.location='https://matrix.org/code';">
     <img src="/images/basic_settings.svg" alt="" className="mxnavsection__icon" />
     <div className="mxnavsection__text">Code</div>
   </a>
