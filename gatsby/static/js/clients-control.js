3152e48aac8caa2f8c9befee313014a53a4cc307
Ben Parsons <ben@bpulse.co.uk>
2019-07-15 19:14:27 +0100

    improve clients page

---
+++ gatsby/static/js/clients-control.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('input[type=radio][name=platform]').change(function() {
+        console.log(this.id);
+        const selectedPlatform = this.id;
+        $('td,th').toArray().forEach(function(cell) {
+            cell = $(cell);
+            const platforms = cell.data("platforms");
+            if (! platforms) return;
+            if (platforms.toLowerCase().indexOf(selectedPlatform) === -1) {
+                cell.hide();
+            } else {
+                cell.show();
+            }
+        });
+    });
+    $("#linux").click();
+});
