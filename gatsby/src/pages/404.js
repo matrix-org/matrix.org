f0a2a9d5e94370fe79af408a59af00cf78d34853
Ben Parsons <ben@bpulse.co.uk>
2020-02-19 15:56:04 +0000

    add custom 404 page

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,32 @@
+/* eslint jsx-a11y/label-has-for:0 */
+
+import React from 'react'
+import Helmet from 'react-helmet'
+import { Layout } from '../components'
+
+import config from '../../config'
+
+
+
+const Legal = () => {
+    return (<Layout hasNavPadding="true">
+            <Helmet title={`404 | ${config.siteTitle}`} />
+        <div>
+            <h1>404, not found</h1>
+            <p>Whatever it is that you want, it's not here. There are, however, things you could do to find what you're looking for.</p>
+            <p><iframe title="DDGSearch" src="https://duckduckgo.com/search.html?width=400&site=matrix.org&prefill=Search Matrix.org"
+      style={{"overflow":"hidden","margin":0,"padding":0,"width":"458px","height":"40px"}} 
+      frameBorder="0"></iframe></p>
+            <p>See some popular pages:
+                <ul>
+                    <li><a href="/">Homepage</a></li>
+                    <li><a href="/blog/posts">Matrix Blog</a></li>
+                    <li><a href="/faq">FAQ</a></li>
+                    <li><a href="https://matrix.org/spec">The Matrix Spec</a></li>
+                </ul>
+            </p>
+        </div>
+    </Layout>)
+}
+
+export default Legal
