556e9c19a3f1368bf393abc8fb4e01f7969e3960
Ben Parsons <ben@bpulse.co.uk>
2020-02-19 16:16:48 +0000

    apply exception to /docs/spec - since gatsby otherwise does not acknowledge the path

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -10,21 +10,22 @@ import config from '../../config'
 
 const Legal = () => {
     return (<Layout hasNavPadding="true">
-            <Helmet title={`404 | ${config.siteTitle}`} />
+            <Helmet title={`404 | ${config.siteTitle}`}>
+                <script type="text/javascript" src="/js/404-catch.js"></script>
+            </Helmet>
         <div>
             <h1>404, not found</h1>
             <p>Whatever it is that you want, it's not here. There are, however, things you could do to find what you're looking for.</p>
             <p><iframe title="DDGSearch" src="https://duckduckgo.com/search.html?width=400&site=matrix.org&prefill=Search Matrix.org"
       style={{"overflow":"hidden","margin":0,"padding":0,"width":"458px","height":"40px"}} 
       frameBorder="0"></iframe></p>
-            <p>See some popular pages:
-                <ul>
-                    <li><a href="/">Homepage</a></li>
-                    <li><a href="/blog/posts">Matrix Blog</a></li>
-                    <li><a href="/faq">FAQ</a></li>
-                    <li><a href="https://matrix.org/spec">The Matrix Spec</a></li>
-                </ul>
-            </p>
+            <p>See some popular pages:</p>
+            <ul>
+                <li><a href="/">Homepage</a></li>
+                <li><a href="/blog/posts">Matrix Blog</a></li>
+                <li><a href="/faq">FAQ</a></li>
+                <li><a href="https://matrix.org/spec">The Matrix Spec</a></li>
+            </ul>
         </div>
     </Layout>)
 }
---
+++ gatsby/static/js/404-catch.js
@@ -0,0 +1,3 @@
+if (window.location.pathname === "/docs/spec") {
+    window.location = window.location;
+}
