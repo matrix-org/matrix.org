01a773a9572c37e8ee20a8554ae1ab58ca06c862
r00ster <r00ster91@protonmail.com>
2022-05-18 18:44:13 +0200

    Use `{" "}` instead of `&nbsp;`

--- gatsby/src/pages/faq.js
+++ gatsby/src/pages/faq.js
@@ -819,7 +819,7 @@ const Faq = ({ data }) => {
                 <a href="/clients">
                   Clients
                 </a>
-                ,&nbsp;
+                ,{" "}
                 <a href="/clients-matrix">
                   Clients Matrix
                 </a>
@@ -834,8 +834,8 @@ const Faq = ({ data }) => {
                 macro.
               </p>
               <p>
-                The most popular and established client is
-                &nbsp;<a href="https://element.io">Element</a>
+                The most popular and established client is{" "}
+                <a href="https://element.io">Element</a>
                 , available on web, desktop, Android and iOS.
               </p>
               <p>Alternatively you can find a client suitable for you:</p>
