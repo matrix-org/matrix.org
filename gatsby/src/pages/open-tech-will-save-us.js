c03640e5c118e586a6cd806204f12639fa7424eb
Thib <thibaultm@matrix.org>
2022-05-25 14:48:14 +0200

    Make OTWSU 16 live

--- gatsby/src/pages/open-tech-will-save-us.js
+++ gatsby/src/pages/open-tech-will-save-us.js
@@ -11,7 +11,7 @@ import moment from "moment";
 
 const title = `Open Tech Will Save Us | ${config.siteTitle}`;
 
-const SHOW_LIVE_STREAM = false;
+const SHOW_LIVE_STREAM = true;
 const NEXT_EVENT = 16;
 
 let liveStream;
@@ -25,7 +25,7 @@ if (SHOW_LIVE_STREAM) {
         controls
       ></video><br />
       <strong><a href="https://stream.matrix.org">Watch on stream.matrix.org</a></strong><br />
-      <strong><a href="https://www.youtube.com/watch?v=dV9BneUu2Yo">Find the stream at https://www.youtube.com/watch?v=dV9BneUu2Yo</a></strong>
+      <strong><a href="https://www.youtube.com/watch?v=H5atH-tbeRo">Find the stream at https://www.youtube.com/watch?v=H5atH-tbeRo</a></strong>
       <script src="/js/hls.light.min.js"></script>
       <script src="/js/livestream.js"></script>
     </div>
