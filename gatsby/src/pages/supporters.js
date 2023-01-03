ab81951eda9d2905e713eb7d996794bc1ec6eeda
Thib <thibaultm@matrix.org>
2023-01-03 14:44:43 +0100

    Update supporters page to reflect donorbox change

--- gatsby/src/pages/supporters.js
+++ gatsby/src/pages/supporters.js
@@ -13,17 +13,27 @@ const Supporters = () => {
             <Helmet title={`Supporters | ${config.siteTitle}`} />
         <div>
             <h1>Supporters</h1>
-            <h2>Thanks to our Patreon supporters!</h2>
+            <h2>Thanks to our community supporters!</h2>
             <div><strong>
                 Alex Gusarev, Alexander Olofsson, asdf, Ivan Shapovalov, Jeroen Bots, Joel Lehtonen,<br />
                 jump_spider, Lance Gaines, lub, Mark Diaz, Paul Lindner, Paul Tötterman,<br />
                 Ross Schulman, Stuart Mumford, Ted Logan, TimeWalker, Timothy Suggs,<br />
                 Travis Ralston, Trung Le, Xiaoyi Chen, Yedige Davletgaliyev, the0, Nicola Fabiano, Michael Downey</strong></div>
-            <h2 className="mxblock__hx">Support Matrix</h2>
-            <p className="mxp">If you share our vision, or are building on top of Matrix, please consider donating...</p>
-            <p className="mxp">Support us on Patreon for great rewards including access to the supporters-only podcast, and even a voice at our weekly meetings.<br /><a href="https://www.patreon.com/matrixdotorg">Find out more at our Patreon page</a>.</p>
-            <a href="https://www.patreon.com/matrixdotorg">
-                <img alt="Patreon" src="/images/patreon.png" />
+            <h2 className="mxblock__hx">Support Matrix</h2>
+            <p className="mxp">If you share our vision, or are building on top of Matrix, please consider donating.</p>
+            <p className="mxp">Support us on Donorbox to allow us to maintain the Matrix specification, maintain Synapse, and much more.</p>
+            <p className="mxp">
+              We keep Patreon for people used to it, but Donorbox allows us to
+              collect donations with much less fees. If you are already using
+              Patreon and don't want to move to Donorbox, it's perfectly fine
+              and we're happy to have you as part of our supporters.
+            </p>
+            <p className="mxp">
+              If you want to start donating, please consider using Donorbox as
+              it's the most efficient way to send us money!
+            </p>
+            <a href="https://donorbox.org/keep-matrix-exciting">
+                <img alt="Donorbox" src="/images/donorbox.png" />
             </a>
         </div>
     </Layout>)
