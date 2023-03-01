92c4a4ac3ad6c8d953ad96db91c678fee3c5228d
Thib <thibaultm@matrix.org>
2023-03-01 13:40:03 +0100

    Add the logic to make the filters work

--- static/js/clients.js
+++ static/js/clients.js
@@ -3,17 +3,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let filtersBar = document.getElementById("filters");
     filtersBar.style.display = "flex";
 
-    class PlatformFilter {
-        constructor(id) {
-            this.makeMenuInteractive(id);
-            this.enableFilters(id);
-            this.filteredOut = [];
+    class Filter {
+        constructor(filterId, isAndFilter, allOf, anyOf) {
+            this.allOf = allOf;
+            this.anyOf = anyOf;
+            this.makeMenuInteractive(filterId);
+            this.enableFilters(filterId);
+            this.isAndFilter = isAndFilter;
         }
 
-        makeMenuInteractive(id) {
-            let filterButton = document.getElementById(id)
+        makeMenuInteractive(filterId) {
+            let filterButton = document.getElementById(filterId)
             filterButton.addEventListener("mouseup", (event) => {
-                let platformFilterMenu = document.getElementById(id+"-menu");
+                let platformFilterMenu = document.getElementById(filterId+"-menu");
                 if(platformFilterMenu.style.display !== "block") {
                     platformFilterMenu.style.display = "block";
                 } else {
@@ -22,65 +24,120 @@ document.addEventListener('DOMContentLoaded', (event) => {
             });
         }
 
-        enableFilters(id) {
-            let platformFilterMenu = document.getElementById(id+"-menu");
+        enableFilters(filterId) {
+            let platformFilterMenu = document.getElementById(filterId+"-menu");
             for(var platformOption of platformFilterMenu.children) {
                 if(!platformOption.classList.contains("reset-links")) {
                     let optionId = platformOption.children[0].id;
                     let checkbox = platformOption.children[0];
+                    if(this.isAndFilter) {
+                        if(checkbox.checked) {
+                            this.allOf.push(platformOption.children[0].id);
+                        }
+                    } else {
+                        if(checkbox.checked) {
+                            this.anyOf.push(platformOption.children[0].id);
+                        }
+                    }
                     checkbox.addEventListener("change", (event) => {
                         if(checkbox.checked) {
-                            this.removeFilter(optionId);
+                            this.boxWasChecked(optionId);
                         } else {
-                            this.addFilter(optionId);
+                            this.boxWasUnchecked(optionId);
                         }
                     });
                 } else {
                     platformOption.children[0].addEventListener("mouseup", (event) => {
-                        this.removeAllFilters(id);
+                        this.checkAllBoxes(filterId);
                     });
                     platformOption.children[1].addEventListener("mouseup", (event) => {
-                        this.addAllFilters(id);
+                        this.uncheckAllBoxes(filterId);
                     });
                 }
             }
         }
 
-        addFilter(optionId) {
-            let filterPos = this.filteredOut.indexOf(optionId);
-            if(filterPos == -1) {
-                this.filteredOut.push(optionId);
-            }
-        }
-
-        removeFilter(optionId) {
-            let filterPos = this.filteredOut.indexOf(optionId);
-            if(filterPos != -1) {
-                this.filteredOut.splice(filterPos, 1);
-            }
-        }
-
-        addAllFilters(id) {
-            let platformFilterMenu = document.getElementById(id+"-menu");
-            for(var platformOption of platformFilterMenu.children) {
-                if(!platformOption.classList.contains("reset-links")) {
-                        platformOption.children[0].checked = false;
-                        this.addFilter(platformOption.children[0].id);
+        boxWasChecked(optionId) {
+            if(this.isAndFilter) {
+                let filterPos = this.allOf.indexOf(optionId);
+                if(filterPos == -1) {
+                    this.allOf.push(optionId);
+                }
+            } else {
+                let filterPos = this.anyOf.indexOf(optionId);
+                if(filterPos == -1) {
+                    this.anyOf.push(optionId);
                 }
             }
+            this.refreshCardsView();
         }
 
-        removeAllFilters(id) {
-            let platformFilterMenu = document.getElementById(id+"-menu");
-            for(var platformOption of platformFilterMenu.children) {
+        boxWasUnchecked(optionId) {
+            if(this.isAndFilter) {
+                let filterPos = this.allOf.indexOf(optionId);
+                if(filterPos != -1) {
+                    this.allOf.splice(filterPos, 1);
+                }
+            } else {
+                let filterPos = this.anyOf.indexOf(optionId);
+                if(filterPos != -1) {
+                    this.anyOf.splice(filterPos, 1);
+                }
+            }
+            this.refreshCardsView();
+        }
+
+        checkAllBoxes(filterId) {
+            let platformFilterMenu = document.getElementById(filterId+"-menu");
+            for(let platformOption of platformFilterMenu.children) {
                 if(!platformOption.classList.contains("reset-links")) {
                         platformOption.children[0].checked = true;
-                        this.removeFilter(platformOption.children[0].id);
+                        this.boxWasChecked(platformOption.children[0].id);
                 }
             }
-            this.filteredOut = [];
         }
+
+        uncheckAllBoxes(filterId) {
+            let platformFilterMenu = document.getElementById(filterId+"-menu");
+            for(let platformOption of platformFilterMenu.children) {
+                if(!platformOption.classList.contains("reset-links")) {
+                        platformOption.children[0].checked = false;
+                        this.boxWasUnchecked(platformOption.children[0].id);
+                }
+            }
+        }
+
+        refreshCardsView() {
+            let clients = document.getElementById("all-clients");
+            for(let client of clients.children) {
+                if(client.classList.contains("client-card")) {
+                    let containsAllOf = this.allOf.every((elem) => {
+                        return client.classList.contains(elem);
+                    });
+                    let containsAnyOf = false;
+                    for(let filter of this.anyOf) {
+                        if(client.classList.contains(filter)) {
+                            containsAnyOf = true;
+                            // and we can break?
+                        }
+                    }
+                    if(this.anyOf.length <= 0) {
+                        containsAnyOf = true;
+                    }
+                    if(containsAllOf && containsAnyOf) {
+                        client.style.display = "flex";
+                    } else {
+                        client.style.display = "none";
+                    }
+                }
+            }
+        };
     }
 
-    let platformFilter = new PlatformFilter("filter-platform");
+    let allOf = [];
+    let anyOf = []
+    let platformFilter = new Filter("filter-platform", true, allOf, anyOf);
+    // let maturityFilter = new Filter("filter-maturity", false, allOf, anyOf);
+    // let licenceFilter = new Filter("filter-licence", false, allOf, anyOf);
+    // let featureFilter = new Filter("filter-features", true, allOf, anyOf);
 })
