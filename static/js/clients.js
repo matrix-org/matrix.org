d19bba5350f093ea2ddbf56497304f6385cfae7c
Thib <thibaultm@matrix.org>
2023-03-02 11:28:19 +0100

    Add example clients, no flicker on load, and use click events

--- static/js/clients.js
+++ static/js/clients.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    // Display filters only if javascript is enabled
-    let filtersBar = document.getElementById("filters");
-    filtersBar.style.display = "flex";
-
     class Filter {
         constructor(filterId, isAndFilter, allOf, anyOf) {
             this.allOf = allOf;
@@ -14,43 +10,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
         makeMenuInteractive(filterId) {
             let filterButton = document.getElementById(filterId)
-            filterButton.addEventListener("mouseup", (event) => {
-                let platformFilterMenu = document.getElementById(filterId+"-menu");
-                if(platformFilterMenu.style.display !== "block") {
-                    platformFilterMenu.style.display = "block";
+            filterButton.addEventListener("click", (event) => {
+                let filterMenu = document.getElementById(filterId + "-menu");
+                if (filterMenu.style.display !== "block") {
+                    filterMenu.style.display = "block";
                 } else {
-                    platformFilterMenu.style.display = "none";
+                    filterMenu.style.display = "none";
                 }
             });
         }
 
         enableFilters(filterId) {
-            let platformFilterMenu = document.getElementById(filterId+"-menu");
-            for(var platformOption of platformFilterMenu.children) {
-                if(!platformOption.classList.contains("reset-links")) {
-                    let optionId = platformOption.children[0].id;
-                    let checkbox = platformOption.children[0];
-                    if(this.isAndFilter) {
-                        if(checkbox.checked) {
+            let filterMenu = document.getElementById(filterId + "-menu");
+            for (const platformOption of filterMenu.children) {
+                if (platformOption.classList.contains("filter-option")) {
+                    const optionId = platformOption.children[0].id;
+                    const checkbox = platformOption.children[0];
+                    if (this.isAndFilter) {
+                        if (checkbox.checked) {
                             this.allOf.push(platformOption.children[0].id);
                         }
                     } else {
-                        if(checkbox.checked) {
+                        if (checkbox.checked) {
                             this.anyOf.push(platformOption.children[0].id);
                         }
                     }
                     checkbox.addEventListener("change", (event) => {
-                        if(checkbox.checked) {
+                        if (checkbox.checked) {
                             this.boxWasChecked(optionId);
                         } else {
                             this.boxWasUnchecked(optionId);
                         }
                     });
-                } else {
-                    platformOption.children[0].addEventListener("mouseup", (event) => {
+                } else if (platformOption.classList.contains("reset-links")) {
+                    platformOption.children[0].addEventListener("click", (event) => {
                         this.checkAllBoxes(filterId);
                     });
-                    platformOption.children[1].addEventListener("mouseup", (event) => {
+                    platformOption.children[1].addEventListener("click", (event) => {
                         this.uncheckAllBoxes(filterId);
                     });
                 }
@@ -58,14 +54,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
         boxWasChecked(optionId) {
-            if(this.isAndFilter) {
+            if (this.isAndFilter) {
                 let filterPos = this.allOf.indexOf(optionId);
-                if(filterPos == -1) {
+                if (filterPos == -1) {
                     this.allOf.push(optionId);
                 }
             } else {
                 let filterPos = this.anyOf.indexOf(optionId);
-                if(filterPos == -1) {
+                if (filterPos == -1) {
                     this.anyOf.push(optionId);
                 }
             }
@@ -73,14 +69,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
         boxWasUnchecked(optionId) {
-            if(this.isAndFilter) {
+            if (this.isAndFilter) {
                 let filterPos = this.allOf.indexOf(optionId);
-                if(filterPos != -1) {
+                if (filterPos != -1) {
                     this.allOf.splice(filterPos, 1);
                 }
             } else {
                 let filterPos = this.anyOf.indexOf(optionId);
-                if(filterPos != -1) {
+                if (filterPos != -1) {
                     this.anyOf.splice(filterPos, 1);
                 }
             }
@@ -88,43 +84,43 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
         checkAllBoxes(filterId) {
-            let platformFilterMenu = document.getElementById(filterId+"-menu");
-            for(let platformOption of platformFilterMenu.children) {
-                if(!platformOption.classList.contains("reset-links")) {
-                        platformOption.children[0].checked = true;
-                        this.boxWasChecked(platformOption.children[0].id);
+            let filterMenu = document.getElementById(filterId + "-menu");
+            for (const platformOption of filterMenu.children) {
+                if (platformOption.classList.contains("filter-option")) {
+                    platformOption.children[0].checked = true;
+                    this.boxWasChecked(platformOption.children[0].id);
                 }
             }
         }
 
         uncheckAllBoxes(filterId) {
-            let platformFilterMenu = document.getElementById(filterId+"-menu");
-            for(let platformOption of platformFilterMenu.children) {
-                if(!platformOption.classList.contains("reset-links")) {
-                        platformOption.children[0].checked = false;
-                        this.boxWasUnchecked(platformOption.children[0].id);
+            let filterMenu = document.getElementById(filterId + "-menu");
+            for (const platformOption of filterMenu.children) {
+                if (platformOption.classList.contains("filter-option")) {
+                    platformOption.children[0].checked = false;
+                    this.boxWasUnchecked(platformOption.children[0].id);
                 }
             }
         }
 
         refreshCardsView() {
             let clients = document.getElementById("all-clients");
-            for(let client of clients.children) {
-                if(client.classList.contains("client-card")) {
+            for (const client of clients.children) {
+                if (client.classList.contains("client-card")) {
                     let containsAllOf = this.allOf.every((elem) => {
                         return client.classList.contains(elem);
                     });
                     let containsAnyOf = false;
-                    for(let filter of this.anyOf) {
-                        if(client.classList.contains(filter)) {
+                    for (const filter of this.anyOf) {
+                        if (client.classList.contains(filter)) {
                             containsAnyOf = true;
                             // and we can break?
                         }
                     }
-                    if(this.anyOf.length <= 0) {
+                    if (this.anyOf.length <= 0) {
                         containsAnyOf = true;
                     }
-                    if(containsAllOf && containsAnyOf) {
+                    if (containsAllOf && containsAnyOf) {
                         client.style.display = "flex";
                     } else {
                         client.style.display = "none";
@@ -140,4 +136,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // let maturityFilter = new Filter("filter-maturity", false, allOf, anyOf);
     // let licenceFilter = new Filter("filter-licence", false, allOf, anyOf);
     // let featureFilter = new Filter("filter-features", true, allOf, anyOf);
+    platformFilter.refreshCardsView();
 })
