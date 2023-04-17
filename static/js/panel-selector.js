57a0e4076a8ba96675ed02c48951ccff4a64dba5
Thib <thibaultm@matrix.org>
2023-04-17 16:47:38 +0200

    Basic javascriptfull version of the panel selector

---
+++ static/js/panel-selector.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    let selector = document.getElementById("sdk-purpose-selector");
+    for (const panelOption of selector.children) {
+        panelOption.addEventListener("change", (event) => {
+            let allPanels = document.getElementsByClassName("panel");
+            for (const panel of allPanels) {
+                panel.classList.add("hidden");
+            }
+            let panel = document.getElementById(panelOption.id + "-panel");
+            panel.classList.remove("hidden");
+        });
+    }
+});
