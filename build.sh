3ed8fbb6d404964b8e5385d2e91c29e055452c06
Laurence Gill <laurenceg@element.io>
2023-06-16 09:11:05 +0100

    Make prod builds work

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # build production using base_url from the site config.toml
-  #zola build
-  zola build --base-url $CF_PAGES_URL
+  zola build
 elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
   # build staging using CF_STAGING_URL env
   zola build --base-url $CF_STAGING_URL
