62eabe43c0abde1803e1d5577f67b6058d665394
MTRNord <mtrnord1@gmail.com>
2024-05-09 17:35:03 +0200

    Make navbar test work on mobile as well

--- tests/menu.spec.ts
+++ tests/menu.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 // We want to make sure that the hover has all children visible and is clickable
-test('can select hovermenu', async ({ page }) => {
+test('can select hovermenu', async ({ page, isMobile }) => {
+    test.skip(isMobile, "Test runs only on desktop");
     await page.goto('/');
 
     await page.getByLabel('Ecosystem').hover();
@@ -16,6 +17,23 @@ test('can select hovermenu', async ({ page }) => {
         // Ensure the elements itself can be hovered
         await element.hover()
         await expect(page.locator(".section-submenu")).toBeVisible()
+        await expect(element).toBeVisible()
+    }
+});
+
+// We want to make sure that the menu is clickable
+test('can select mobile menu', async ({ page, isMobile }) => {
+    test.skip(!isMobile, "Test runs only on mobile");
+    await page.goto('/');
+    await page.locator(".dropdown-button").click();
+
+    await page.getByLabel('Ecosystem').click();
+    const submenu = page.locator(".section-submenu");
+
+    const elements = await submenu.all();
+
+    await expect(page.locator(".section-submenu")).toBeVisible()
+    for (const element of elements) {
         await expect(element).toBeVisible()
     }
 });
